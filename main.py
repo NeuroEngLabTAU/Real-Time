fb1c608a3c82b20d06e0972bd06ede5d7cf30cfc
dvirbd <dvirbd613@gmail.com>
2023-07-19 12:30:32 +0300

    added the video option

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import os
 import datetime
 import time
 import keyboard
+import sys
 
 #########################
 ###### Load image #######
@@ -102,30 +103,92 @@ def fill_polygon(vertices, num_rows, num_cols):
     return result
 
 
+# function for video recording
+def record_video(webcam):
+    global is_recording_video, output_filename
+
+    # Get the current time including milliseconds
+    recording_start_time = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S-%f")[:-3]
+    output_filename = f"video_{recording_start_time}.avi"
+    print(f"Video recording started at {recording_start_time} ...")
+
+    # Define the video writer object
+    out = cv2.VideoWriter(output_filename, fourcc, 30.0, (640, 480))
+
+    # Start recording until user interrupts or is_recording is False
+    while is_recording_video:
+        ret, frame = webcam.read()  # Read frame from webcam
+
+        if ret:
+            # Write the frame to the output video file
+            out.write(frame)
+        else:
+            print("Failed to capture frame")
+            break
+
+    print("Video recording stopped at: ", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+    # Release the video writer object
+    out.release()
+
+
+# functions to control video recording
+def start_video(data, cap):
+    global is_recording_video
+    if not is_recording_video:
+        is_recording_video = True
+        record_thread = threading.Thread(target=record_video, args=(cap,))
+        record_thread.start()
+        data.add_annotation('started video recording')
+
+
+def stop_video(data):
+    global is_recording_video
+    if is_recording_video:
+        is_recording_video = False
+        data.add_annotation('stopped video recording')
+
 if __name__ == '__main__':
 
     # define the desired visualisation
     viz_raw = False  # Aaron's real time raw data plotting
     # viz_ica = False  # Bara's semi real-time ICA
-    viz_ica_streaming = True  # streaming ICA signals with heatmaps
-    Electrodes_raw = False  # raw signal electrodes
+    viz_ica_streaming = False  # streaming ICA signals with heatmaps
+    Electrodes_raw = True  # raw signal electrodes
 
-    ica_integration_time = 15  # seconds
+    ica_integration_time = 10  # seconds
     stop_ica = False  # if True the ica will be calculated for all the recorded data (ica_integration_time will be ignored),
     # until the button 'ICA converged!' is pressed, then the data will be plotted according to the unmixing matrix
     # that was calculated last (when the button 'ICA converged!' was pressed)
     window_secs = 10  # seconds
 
+    # if usage of video recording is desired:
+    # takes about 20 seconds to connect to the webcam at the beginning
+    video = True  # if True video recording is controlled by the space bar for start and 'q' for stop
+    one_video = True  # starts the first video recording with the EMG recording,
+    # also can set only one_video to True if only one video of the whole EMG recording is desired
+
+    if video or one_video:
+        print("connecting to webcam...")
+        cap = cv2.VideoCapture(0)
+        print("connected to webcam at: ", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+        # Check if the webcam is opened successfully
+        if not cap.isOpened():
+            print("Failed to open webcam")
+            cap.release()  # Release the webcam resource
+            sys.exit()
+
+        # Define the video writer object
+        fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        output_filename = ""
+        is_recording_video = False
+
+
     if viz_ica_streaming or Electrodes_raw:
         #upload your image - insert image path
         image_path = r"C:\Users\YH006_new\Simulation\Paul_45.jpg"
-
-        # # Get the path of the current script
-        # script_path = os.path.abspath(__file__)
-        # # Get the directory containing the script
-        # script_directory = os.path.dirname(script_path)
-        # # Construct the image path relative to the script directory
-        # image_path = os.path.join(script_directory, "face-muscles-anatomy.jpg")
+        # image_path = 'face-muscles-anatomy.jpg'
 
         image, height, width = image_load(image_path)
 
@@ -145,6 +208,7 @@ if __name__ == '__main__':
         num_rows, num_cols = height, width
         d_interpolate = fill_polygon(vertices, num_rows, num_cols)
 
+    # prepare the data object
     host_name = "127.0.0.1"
     port = 20001
     n_bytes = 1024
@@ -158,11 +222,16 @@ if __name__ == '__main__':
     print('started data recording at: ', start_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
     data.add_annotation("Start recording")
 
-    filters = {'highpass': {'W': 30}, 'comb': {'W': 50}}
+    if video:
+        # press 'space' to start the video recording and 'q' to stop it
+        keyboard.on_press_key("space", lambda _: start_video(data, cap))
+        keyboard.on_press_key("q", lambda _: stop_video(data))
 
-    # if viz_raw:
-    #     viz = Viz(data, window_secs=window_secs, plot_exg=True, plot_imu=True, plot_ica=False, find_emg=False, filters=filters,
-    #               update_interval_ms=10, ylim_exg=(-250, 250), max_points=None, max_timeout=15, filter_data=True)
+    if one_video:
+        start_video(data, cap)
+
+
+    filters = {'highpass': {'W': 30}, 'comb': {'W': 50}}
 
     if viz_raw:
         original_viz = Viz(data, window_secs=window_secs, plot_exg=True, plot_imu=True, plot_ica=False, find_emg=False, filters=filters,
@@ -176,10 +245,10 @@ if __name__ == '__main__':
     #               x_coor=x_coor, y_coor=y_coor, width=width, height=height, image=image, filter_data=True)
 
     if viz_ica_streaming:
-        ica_viz = Viz_ICA_Streaming(data, window_secs=window_secs, ica_integration_time=ica_integration_time, stop_ica = stop_ica,
+        ica_viz = Viz_ICA_Streaming(data, window_secs=window_secs, ica_integration_time=ica_integration_time, stop_ica=stop_ica,
                                     plot_exg=True, plot_imu=False, plot_ica=False, find_emg=False, filters=filters,
-                  update_interval_ms=400, ylim_exg=(-5, 5), max_points=None, max_timeout=15,
-                  x_coor=x_coor, y_coor=y_coor, width=width, height=height, image=image, d_interpolate=d_interpolate, filter_data=True)
+                                    update_interval_ms=200, ylim_exg=(-5, 5), max_points=None, max_timeout=15,
+                                    x_coor=x_coor, y_coor=y_coor, width=width, height=height, image=image, d_interpolate=d_interpolate, filter_data=True)
 
         ica_viz.start()
 
@@ -203,6 +272,9 @@ if __name__ == '__main__':
         if keyboard.is_pressed('esc'):
             break
         time.sleep(0.1)
+
+    if (video or one_video) and is_recording_video:  # if recording is on, stop the video
+        stop_video(data)
 
     total_time = datetime.datetime.now() - start_time
     data.add_annotation("Stop recording at " + f"{int(total_time.total_seconds() // 60):02d}:"
