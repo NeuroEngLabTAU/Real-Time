ef5edb5bd241ce60347c56639860ac9bd02737c3
Rawan Ibrahim <hava1@mail.tau.ac.il>
2025-04-22 21:11:03 +0300

    Clean up and refactor the main script.
    - Simplified the main script by consolidating visualization logic, removing unused code, and cleaning up redundant configurations.
    - Keep only the raw data Viz, other visualizations moved to the 'advanced_viz' branch for further development until it is ready to use.
    - Added LSL inlet setup for trigger stream and refined electrode visualization filtering.

--- main.py
+++ main.py
@@ -1,188 +1,71 @@
 import matplotlib
-# matplotlib.use('Qt5Agg')
-import matplotlib.pyplot as plt
-from PyQt5.QtCore import QThread, Qt, pyqtSignal, QTimer
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import threading
+import pandas as pd
+from pylsl.pylsl import resolve_stream, StreamInlet
 
-from XtrRT.data import Data  #data collection
-from XtrRT.viz import Viz  #real time raw data plotting
-# from XtrRT.ica import Viz_ICA   # interactive ica in real time
-from XtrRT.ica_streaming import Viz_ICA_Streaming    # Streaming ICA
+from XtrRT.facial_image import Y_COOR, X_COOR, image_load, IMAGE_PATH
+
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+
+from XtrRT.data import Data  # data collection
 from XtrRT.electrodes_raw_streaming import Electrodes_Raw_Streaming  # stream raw data with electrodes location
 
-    # wait for a key to be pressed to exit
-    cv2.waitKey(0)
+# Define the desired visualisation
+VIZ_RAW = True  # Raw signal with facial electrodes
+SAMPLING_RATE_Hz = 500  # Sampling rate in Hz
+VIZ_FILTERS = {'bandpass': [30, SAMPLING_RATE_Hz / 2 - 1], 'comb': [50, 100]}
 
-    # close the window
-    cv2.destroyAllWindows()
-
-
-# create a dummy heatmap
-def fill_polygon(vertices, num_rows, num_cols):
-    # Create a grid of points
-    x = np.linspace(0, num_cols-1, num_cols)
-    y = np.linspace(0, num_rows-1, num_rows)
-    xx, yy = np.meshgrid(x, y)
-    points = np.vstack((xx.flatten(), yy.flatten())).T
-
-    # Create a path from the polygon vertices
-    path = Path(vertices)
-
-    # Determine whether each point is inside the polygon
-    mask = path.contains_points(points)
-    mask = mask.reshape(num_rows, num_cols)
-
-    # Fill the grid with random values between 0 and 1, and NaN values outside the polygon
-    result = np.empty((num_rows, num_cols))
-    result[~mask] = np.nan
-    result[mask] = np.random.rand(np.sum(mask))
-
-    return result
-
+# Define LSL parameters
+IS_LSL_INLET = False  # LSL inlet for trigger stream
+LSL_INLET_NAME = "XtrRT Inlet"  # Name of the LSL inlet stream
 
 if __name__ == '__main__':
+    lsl_inlet = None
+    if IS_LSL_INLET:
+        # Initialize LSL inlet for "MyTriggerStream"
+        try:
+            # Adjust this to match the stream from 'experiment_waldo_intensity.py'
+            streams = resolve_stream('name', LSL_INLET_NAME)  # Resolving by stream name
+            lsl_inlet = StreamInlet(streams[0])
+            print(f"LSL inlet '{LSL_INLET_NAME}' detected.")
+        except Exception as e:
+            print("Error initializing LSL inlet:", e)
 
-    # define the desired visualisation
-    viz_raw = False  # Aaron's real time raw data plotting
-    viz_spectogram = True  # spectogram streaming
-    # viz_ica = False  # Bara's semi real-time ICA
-    viz_ica_streaming = False  # streaming ICA signals with heatmaps
-    Electrodes_raw = False  # raw signal electrodes
+    image, height, width = image_load(IMAGE_PATH)
 
-    ica_integration_time = 10  # seconds
-    stop_ica = False  # if True the ica will be calculated for all the recorded data (ica_integration_time will be ignored),
-    # until the button 'ICA converged!' is pressed, then the data will be plotted according to the unmixing matrix
-    # that was calculated last (when the button 'ICA converged!' was pressed)
-    window_secs = 10  # seconds
-
-    # if usage of video recording is desired:
-    # takes about 20 seconds to connect to the webcam at the beginning
-    video = False  # if True video recording is controlled by the space bar for start and 'q' for stop
-    one_video = True  # starts the first video recording with the EMG recording,
-    # also can set only one_video to True if only one video of the whole EMG recording is desired
-
-    if video or one_video:
-        print("connecting to webcam...")
-        cap = cv2.VideoCapture(0)
-        print("connected to webcam at: ", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
-
-        # Check if the webcam is opened successfully
-        if not cap.isOpened():
-            print("Failed to open webcam")
-            cap.release()  # Release the webcam resource
-            sys.exit()
-
-        # Define the video writer object
-        fourcc = cv2.VideoWriter_fourcc(*"XVID")
-        output_filename = ""
-        is_recording_video = threading.Event()
-
-
-    if viz_ica_streaming or Electrodes_raw:
-        #upload your image - insert image path
-        # image_path = r"C:\Users\YH006_new\Simulation\Paul_45.jpg"
-        # Get the path of the current script
-        script_path = os.path.abspath(__file__)
-        # Get the directory containing the script
-        script_directory = os.path.dirname(script_path)
-        # Construct the image path relative to the script directory
-        image_path = os.path.join(script_directory, "face-muscles-anatomy.jpg")
-
-        image, height, width = image_load(image_path)
-
-
-        #to select the electrodes locations on the image leave empty, otherwise load
-        # x_coor=[]
-        # y_coor=[]
-        x_coor = [557, 398, 336, 444, 466, 342, 389, 490, 601, 450, 335, 328, 422, 545, 551, 689]
-        y_coor = [836, 786, 690, 721, 657, 634, 586, 599, 567, 541, 537, 477, 387, 381, 290, 289]
-        # x_coor = np.load(r'C:\Users\YH006_new\Simulation\x_coor_45.npy')
-        # y_coor = np.load(r'C:\Users\YH006_new\Simulation\y_coor_45.npy')
-        if len(x_coor)==0:  # if manually inserted coordinates, there is not need to get the electrode locations (otherwise select electrodes from image)
-            get_location()
-
-        # create dummy heatmap for funcAnimator setup
-        vertices = list(zip(x_coor, y_coor))
-        num_rows, num_cols = height, width
-        d_interpolate = fill_polygon(vertices, num_rows, num_cols)
-
-    # prepare the data object
+    # Prepare the data object
     host_name = "127.0.0.1"
     port = 20001
     n_bytes = 1024
-    data = Data(host_name, port, verbose=False, timeout_secs=15, save_as="test.edf")
+    data = Data(host_name, port, verbose=False, timeout_secs=15, save_as="test.edf",
+                lsl_inlet=lsl_inlet)
     data.start()
-
-    while not data.has_data:  # Wait to start collecting data before doing anything
-        continue
 
     data.add_annotation("Start recording")
 
-    if video:
-        # press 'space' to start the video recording and 'q' to stop it
-        keyboard.on_press_key("space", lambda _: start_video(data, cap))
-        keyboard.on_press_key("q", lambda _: stop_video(data))
-
-    if one_video:
-        start_video(cap, is_recording_video, fourcc, output_filename)
-
-
-    filters = {'highpass': {'W': 30}, 'comb': {'W': 50}}
-
-    if viz_raw:
-        # raw_fig = plt.figure()
-        raw_streaming = Viz(data, window_secs=10, plot_exg=True, plot_imu=False, plot_ica=False, find_emg=False, filters=filters,
-                  update_interval_ms=10, ylim_exg=(-250, 250), max_points=None, max_timeout=15)  # in old_viz there is also "filter_data"
-        raw_viz = raw_streaming.start()
-
-    if viz_spectogram:
-        # spec_fig = plt.figure()
-        spec_streaming = Viz_spec(data, window_secs=10, plot_exg=True, plot_spectogram=True, find_emg=False, filters=filters,
-                  update_interval_ms=10, ylim_exg=(-350, 350), max_points=None, max_timeout=15, filter_data=False)
-        spec_viz = spec_streaming.start()
-    # if viz_ica:
-    #     viz = Viz_ICA(data, window_secs=10, plot_exg=True, plot_imu=False, plot_ica=False, find_emg=False, filters=filters,
-    #               update_interval_ms=10, ylim_exg=(-250, 250), max_points=None, max_timeout=15,
-    #               x_coor=x_coor, y_coor=y_coor, width=width, height=height, image=image, filter_data=True)
-
-    if viz_ica_streaming:
-        ica_fig = plt.figure()
-        viz = Viz_ICA_Streaming(data, window_secs=10, plot_exg=True, plot_imu=False, plot_ica=False, find_emg=False, filters=filters,
-                  update_interval_ms=10, ylim_exg=(-5, 5), max_points=None, max_timeout=15,
-                  x_coor=x_coor, y_coor=y_coor, width=width, height=height, image=image, d_interpolate=d_interpolate, filter_data=True)
-        #figure=ica_fig
-        ica_viz = viz.start()
-
-
-    if Electrodes_raw:
+    if VIZ_RAW:
         electrodes_fig, electrodes_axes = plt.subplots()
-        elctrodes_streaming = Electrodes_Raw_Streaming(data, window_secs=2.5, plot_exg=True, plot_imu=False, plot_ica=False,
-                   find_emg=False, filters=filters, update_interval_ms=100, ylim_exg=(-250, 250), max_points=None, max_timeout=15,
-                   x_coor=x_coor, y_coor=y_coor, width=width, height=height, image=image, filter_data=True,
+        elctrodes_streaming = Electrodes_Raw_Streaming(data, window_secs=2.5, plot_exg=True, plot_imu=False,
+                                                       filters=VIZ_FILTERS, update_interval_ms=100,
+                                                       ylim_exg=(-500, 500), max_points=None, max_timeout=15,
+                                                       x_coor=X_COOR, y_coor=Y_COOR, width=width, height=height,
+                                                       image=image, filter_data=True,
                                                        figure=electrodes_fig, axes=electrodes_axes)
 
         electrodes_viz = elctrodes_streaming.start()
 
-    if viz_raw or viz_ica_streaming or Electrodes_raw or viz_spectogram:
-        plt.show()
+    plt.show()
 
-    time.sleep(1)
-    # to stop the recording press esc
-    print('press \'esc\' to stop data recording')
-
-    while True:
-        if keyboard.is_pressed('esc'):
-            break
-        time.sleep(0.1)
-
-    if (video or one_video) and is_recording_video:  # if recording is still on, stop the video
-        stop_video(is_recording_video)
-
-    data.add_annotation("data.start_time: " + str(data.start_time))
     data.add_annotation("Stop recording")
     data.stop()
-    print('stopped data recording at: ', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+    data.join()  # Wait for the data thread to finish
+    print('Main code terminated')
 
     print(data.annotations)
-    print('process terminated')
+    print('Process terminated')
+
+    # Export the annotations to a CSV file
+    df_annotations = pd.DataFrame(data.annotations, columns=['Onset', 'Duration', 'Description'])
+    df_annotations.to_csv(f'{data.save_as}_annotations.csv', index=False)
+    print('Annotations exported to CSV')
